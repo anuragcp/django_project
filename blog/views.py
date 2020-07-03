df605739a80dfe8f3205c0a4b40ab3ee63e55a58
anuragcp <anudeveloper777@gmail.com>
2020-07-03 20:11:16 +0530

    created templates and dynamic title for about and blog page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# dummy data
+posts = [
+    {
+        'author':'Anurag',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27 2018'
+    },
+    {
+        'author':'Shyamrag',
+        'title': 'Blog Post 2',
+        'content': 'Second things',
+        'date_posted': 'August 28 2018'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context=context)
+
+def about(request):
+    return render(request, 'blog/about.html', context={'title': 'About'})
